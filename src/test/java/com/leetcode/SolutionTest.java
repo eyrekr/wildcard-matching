4ef806132b3266b65deddaefc87052b2811048e7
Erik Kratochvil <erik@sapho.com>
2020-01-25 12:34:24 +0100

    Change the test method source.

--- src/test/java/com/leetcode/SolutionTest.java
+++ src/test/java/com/leetcode/SolutionTest.java
@@ -1,25 +1,40 @@
 package com.leetcode;
 
+import java.util.stream.Stream;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class SolutionTest {
 
+    private static Stream<Arguments> isMatch() {
+        return Stream.of(
+                Arguments.of("adceb", "*a*b", true),
+                Arguments.of("aab", "c*a*b", false),
+                Arguments.of("mississippi", "m??*ss*?i*pi", false),
+                Arguments.of("acdcb", "a*c?b", false),
+                Arguments.of("", "", true),
+                Arguments.of("", "*", true),
+                Arguments.of("", "a", false),
+                Arguments.of("axxxxxaxxxxxa", "a*a", true),
+                Arguments.of(
+                        "aaaabaaaabbbbaabbbaabbaababbabbaaaababaaabbbbbbaabbbabababbaaabaabaaaaaabbaabbbbaababbababaabbbaababbbba",
+                        "*****b*aba***babaa*bbaba***a*aaba*b*aa**a*b**ba***a*a*",
+                        true));
+    }
+
     @ParameterizedTest
-    @CsvSource({"adceb,*a*b,true", "aab,c*a*b,false", "mississippi,m??*ss*?i*pi,false", "acdcb,a*c?b,false", ",,true",
-            ",*,true", "a,,false",
-            "aaaabaaaabbbbaabbbaabbaababbabbaaaababaaabbbbbbaabbbabababbaaabaabaaaaaabbaabbbbaababbababaabbbaababbbba,*****b*aba***babaa*bbaba***a*aaba*b*aa**a*b**ba***a*a*,true"})
+    @MethodSource
     void isMatch(String input, String pattern, boolean expectedAnswer) {
-        if (input == null) {
-            input = "";
-        }
-        if (pattern == null) {
-            pattern = "";
-        }
         final boolean actualAnswer = new Solution().isMatch(input, pattern);
         Assertions.assertThat(actualAnswer)
-                .as("Expected '%s' matches '%s' to be %b but was %b", input, pattern, expectedAnswer, actualAnswer)
+                .as("'%s' should %s '%s' but it %s",
+                        input,
+                        expectedAnswer ? "match" : "not match",
+                        pattern,
+                        actualAnswer ? "did" : "didn't")
                 .isEqualTo(expectedAnswer);
     }
 }
