b865333adecc76922b8be82656e0153346bd5ef2
Erik Kratochvil <erik@sapho.com>
2020-01-25 14:43:09 +0100

    Linear solution O(m+n).

--- src/main/java/com/leetcode/Solution.java
+++ src/main/java/com/leetcode/Solution.java
@@ -3,34 +3,33 @@ package com.leetcode;
 public class Solution {
 
     public boolean isMatch(String input, String pattern) {
-        final int n = input.length();
+        int i = 0;
+        int p = 0;
+        int lastStarInPattern = -1;
+        int returnToI = 0;
 
-        boolean[] data = new boolean[n + 1];
-        data[0] = true;
-
-        for (int k = 0; k < pattern.length(); k++) {
-            char p = pattern.charAt(k);
-            boolean atLeastOneMatches = false;
-            if (p == '*') {
-                for (int i = 0; i <= n; i++) {
-                    data[i] = atLeastOneMatches || data[i];
-                    atLeastOneMatches |= data[i];
-                }
+        while (i < input.length()) {
+            if (p < pattern.length()
+                    && (pattern.charAt(p) == '?' || pattern.charAt(p) == input.charAt(i))) {
+                i++;
+                p++;
+            } else if (p < pattern.length() && pattern.charAt(p) == '*') {
+                lastStarInPattern = p;
+                returnToI = i;
+                p++;
+            } else if (lastStarInPattern != -1) {
+                p = lastStarInPattern + 1;
+                returnToI++;
+                i = returnToI;
             } else {
-                boolean prev = data[0];
-                for (int i = 1; i <= n; i++) {
-                    final boolean matches = prev && (p == '?' || p == input.charAt(i - 1));
-                    prev = data[i];
-                    data[i] = matches;
-                    atLeastOneMatches |= matches;
-                }
-                data[0] = false;
-            }
-            if (!atLeastOneMatches) {
                 return false;
             }
         }
 
-        return data[n];
+        while (p < pattern.length() && pattern.charAt(p) == '*') {
+            p++;
+        }
+
+        return p == pattern.length();
     }
 }
