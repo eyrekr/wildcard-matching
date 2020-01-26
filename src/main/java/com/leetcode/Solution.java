a7056cffc766a99c0f813594c07e69f17d67e45c
Erik Kratochvil <erik@sapho.com>
2020-01-26 14:50:10 +0100

    Conversion to byte.

--- src/main/java/com/leetcode/Solution.java
+++ src/main/java/com/leetcode/Solution.java
@@ -5,33 +5,34 @@ public class Solution {
     public boolean isMatch(String input, String pattern) {
         final int n = input.length();
 
-        boolean[] matches = new boolean[n + 1];
-        matches[0] = true;
-        boolean asterisk = false;
+        final byte[] matches = new byte[n + 1];
+        matches[0] = 1;
+        byte asterisk = 0;
         int liveZone = 0;
 
         for (int k = 0; k < pattern.length(); k++) {
             char p = pattern.charAt(k);
             if (p == '*') {
-                asterisk = true;
+                asterisk = 1;
             } else {
                 final int stop = liveZone;
                 liveZone = Integer.MAX_VALUE;
                 for (int i = n; i > stop; i--) {
-                    final boolean match = (asterisk || matches[i - 1])
-                            && (p == '?' || p == input.charAt(i - 1));
-                    matches[i] = match;
-                    if (match) {
+                    final byte match = (byte) (asterisk | matches[i - 1]);
+                    if (match == 1 && (p == '?' || p == input.charAt(i - 1))) {
+                        matches[i] = 1;
                         liveZone = i;
+                    } else {
+                        matches[i] = 0;
                     }
                 }
-                asterisk = false;
+                asterisk = 0;
                 if (liveZone == Integer.MAX_VALUE) {
                     return false;
                 }
             }
         }
 
-        return matches[n] || asterisk;
+        return (matches[n] | asterisk) == 1;
     }
 }
